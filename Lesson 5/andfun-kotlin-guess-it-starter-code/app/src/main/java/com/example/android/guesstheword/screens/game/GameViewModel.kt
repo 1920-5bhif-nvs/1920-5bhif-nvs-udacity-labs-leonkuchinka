67c21da5e056c03510b1fae46c202ac71cf03896
leonkuchinka <l.kuchinka@gmail.com>
2019-12-13 12:12:52 +0100

    5.10 Exercise: Populate the GameViewModel - Leon Kuchinka

--- Lesson 5/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lesson 5/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -3,18 +3,61 @@ package com.example.android.guesstheword.screens.game;
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
-
-/**
- * ViewModel containing all the logic needed to run the game
- */
 class GameViewModel : ViewModel() {
 
+        var word = ""
+
+        var score = 0
+
+        private lateinit var wordList: MutableList<String>
+
+
         init {
-        Log.i("GameViewModel", "GameViewModel created!")
+                resetList()
+                nextWord()
         }
 
-        override fun onCleared() {
-        super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed!")
+        private fun resetList() {
+                wordList = mutableListOf(
+                        "queen",
+                        "hospital",
+                        "basketball",
+                        "cat",
+                        "change",
+                        "snail",
+                        "soup",
+                        "calendar",
+                        "sad",
+                        "desk",
+                        "guitar",
+                        "home",
+                        "railway",
+                        "zebra",
+                        "jelly",
+                        "car",
+                        "crow",
+                        "trade",
+                        "bag",
+                        "roll",
+                        "bubble"
+                )
+                wordList.shuffle()
+        }
+
+
+        private fun nextWord() {
+                if (wordList.isEmpty()) {
+                } else {
+                        word = wordList.removeAt(0)
+                }
+        }
+        fun onSkip() {
+                score--
+                nextWord()
+        }
+
+        fun onCorrect() {
+                score++
+                nextWord()
         }
 }
