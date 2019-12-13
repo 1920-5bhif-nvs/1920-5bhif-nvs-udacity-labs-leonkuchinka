3031c26fe3b0809e14c81dd703a7d73f32d7ece8
leonkuchinka <l.kuchinka@gmail.com>
2019-12-13 12:17:54 +0100

    5.16 Exercise: Add LiveData Encapsulation to GameViewModel - Leon Kuchinka

--- Lesson 5/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lesson 5/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,22 +1,28 @@
 package com.example.android.guesstheword.screens.game;
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-        val word = MutableLiveData<String>()
+        private val _word = MutableLiveData<String>()
+        val word: LiveData<String>
+                get() = _word
 
-        val score = MutableLiveData<Int>()
+
+        private val _score = MutableLiveData<Int>()
+        val score: LiveData<Int>
+                get() = _score
+
 
         private lateinit var wordList: MutableList<String>
-
 
         init {
                 resetList()
                 nextWord()
-                score.value = 0
+                _score.value = 0
         }
 
         private fun resetList() {
@@ -49,17 +55,17 @@ class GameViewModel : ViewModel() {
         private fun nextWord() {
                 if (wordList.isEmpty()) {
                 } else {
-                        word.value = wordList.removeAt(0)
+                        _word.value = wordList.removeAt(0)
                 }
         }
 
         fun onSkip() {
-                score.value = (score.value)?.minus(1)
+                _score.value = (_score.value)?.minus(1)
                 nextWord()
         }
 
         fun onCorrect() {
-                score.value = (score.value)?.plus(1)
+                _score.value = (_score.value)?.plus(1)
                 nextWord()
         }
 }
